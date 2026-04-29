fc93e93db8a828bbab5100a2b3f177e3a08b7737
ezhang316 <110512351+ezhang316@users.noreply.github.com>
2026-04-29 22:54:44 +1000

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.py
@@ -0,0 +1,28 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+from operator import attrgetter
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+
+        max_rooms = 0
+
+        min_heap = []
+
+        intervals.sort(key=attrgetter('start', 'end'))
+        for i in intervals:
+
+            if min_heap:
+                while min_heap[0] < i.start:
+                    heapq.heappop(min_heap)
+
+            heapq.heappush(min_heap, i.end)
+                
+            max_rooms = max(max_rooms, len(min_heap))
+        
+        return max_rooms
