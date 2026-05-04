fe5cdc557202cd51a4769a71ebabe8c8e5ba3907
ezhang316 <110512351+ezhang316@users.noreply.github.com>
2026-05-04 19:39:32 +1000

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        can = {}
+
+        def rec(index):
+            if index >= len(nums) - 1:
+                return True
+            if index not in can:
+                for i in range(nums[index], 0, -1):
+                    if index + i in can:
+                        return can[index + 1]
+                    can[index + i] = ret = rec(index + i)
+                    if ret:
+                        return ret
+            else:
+                return can[index]
+            can[index] = False
+            return False
+        
+        return rec(0)
+
