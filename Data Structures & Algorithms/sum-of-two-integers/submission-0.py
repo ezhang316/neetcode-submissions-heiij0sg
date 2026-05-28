c2805e8de431965a10142dfb34794d2a1f3563cc
ezhang316 <110512351+ezhang316@users.noreply.github.com>
2026-05-28 21:33:25 +1000

    Add: sum-of-two-integers - submission-0

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        carry = 0
+        res = 0
+        for i in range(32):
+            aBit = (a >> i) & 1
+            bBit = (b >> i) & 1
+            
+            sum_bit = aBit ^ bBit ^ carry
+            carry = (aBit + bBit + carry) >= 2
+
+            res |= sum_bit << i
+
+        mask = 0xFFFFFFFF
+
+        if res > 0x7FFFFFFF:
+            res = ~(res ^ mask)
+        
+        return res
